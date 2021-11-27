ad541a44588994378c53f4a9cbada8f23b281383
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-11-27 15:35:16 +0100

    Correct class path and delete unused config

--- sports_scheduling/log/__init__.py
+++ sports_scheduling/log/__init__.py
@@ -1,11 +1,8 @@
 import logging.config
 
-from sports_scheduling.log.application_logging import LOGGING_CONF
-
 logger = logging.getLogger('sports-time-scheduling')
 
 
 def init_logging():
     logging.config.fileConfig('log/logging.conf')
-
     return logging
