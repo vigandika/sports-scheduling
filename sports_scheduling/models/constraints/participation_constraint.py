d969917e82f94dc3b01733bcfabdd6168d34558f
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-11-28 23:02:32 +0100

    Implement participation constraint

--- sports_scheduling/models/constraints/participation_constraint.py
+++ sports_scheduling/models/constraints/participation_constraint.py
@@ -1,4 +1,9 @@
+from typing import List
+
+from numpy import ndarray
+
 from sports_scheduling.models.constraints.base_constraint import BaseConstraint
+from sports_scheduling.models.teams.teams import Team
 
 
 class ParticipationConstraint(BaseConstraint):
@@ -7,5 +12,13 @@ class ParticipationConstraint(BaseConstraint):
         """Every team should play exactly once at each matchweek"""
         super().__init__(bracket='participationConstraint', level='HARD')
 
-    def is_violated(self):
-        pass
+    def is_violated(self, teams: List[Team], fixture_table: ndarray):
+        no_of_matchweeks = (len(teams) - 1) * 2
+        for team in teams:
+            assert getattr(team, 'assigned_index') is not None, f"team {team.name} needs to have an 'assigned_index'"
+            for matchweek in range(1, no_of_matchweeks):
+                if matchweek not in fixture_table[team.assigned_index] and matchweek not in fixture_table[:, team.assigned_index]:
+                    # If the matchweek is not found in home fixtures (row) or in away fixtures (column)
+                    return True
+
+        return False
