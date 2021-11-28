36ea2d54a15406a73eb9666f6070bbe3420fe8cf
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-11-28 23:51:25 +0100

    Implement and test shared venue constraints

--- sports_scheduling/models/constraints/shared_venue_constraint.py
+++ sports_scheduling/models/constraints/shared_venue_constraint.py
@@ -1,15 +1,41 @@
 from typing import List, Tuple
 
+from numpy import ndarray
+
 from sports_scheduling.models.constraints.base_constraint import BaseConstraint
+from sports_scheduling.models.teams.teams import Team
+from sports_scheduling.util import get_team_by_id
 
 
 class SharedVenueConstraint(BaseConstraint):
 
-    def __init__(self, shared_venue_team_pairs: List[Tuple[str, str]]):
-        """The pair of teams in shared venue constraint should have complementary H-A patterns"""
+    def __init__(self, shared_venue_team_pairs: List[Tuple[int, int]]):
+        """
+        The pair of teams in shared venue constraint should have complementary H-A patterns
+
+        :param shared_venue_team_pairs: a list containing pairs of id-s of the shared venue teams e.g. [(1,3), (2,4)] means that the pair of
+                                        teams with ids 1 and 3 and the pair of teams with ids 2 and 4 are shared venue teams.
+        """
         super().__init__(bracket='sharedVenueConstraint', level='HARD')
         self.no_of_shared_venue_teams = len(shared_venue_team_pairs)
         self.shared_venue_team_pairs = shared_venue_team_pairs
 
-    def is_violated(self):
-        pass
+    def is_violated(self, teams: List[Team], fixture_table: ndarray):
+        for team_pair in self.shared_venue_team_pairs:
+            team_1 = get_team_by_id(teams, team_pair[0])
+            team_2 = get_team_by_id(teams, team_pair[1])
+            assert getattr(team_1, 'assigned_index') is not None and getattr(team_2, 'assigned_index') is not None, \
+                f"either team with id {team_1.id} or team with id {team_2.id} have no assigned_index property"
+
+            # If one of the matchweeks in team1's home games (row schedule) is in team2's home games, the constraint is violated
+            for matchweek in fixture_table[team_1.assigned_index]:
+                if matchweek in fixture_table[team_2.assigned_index] and matchweek != 0:
+                    return True
+
+            # If one of the matchweeks in team1's away games (column schedule) is in team2's away games?(maybe removed),
+            # the constraint is violated
+            for matchweek in fixture_table[:, team_1.assigned_index]:
+                if matchweek in fixture_table[:, team_2.assigned_index] and matchweek != 0:
+                    return True
+
+            return False
---
+++ tests/unit/shared_venue_constraint_tests.py
@@ -0,0 +1,56 @@
+from unittest import TestCase
+
+import numpy as np
+
+from sports_scheduling.models.constraints.shared_venue_constraint import SharedVenueConstraint
+from sports_scheduling.models.teams.teams import Team
+
+
+class SharedVenueConstraintTests(TestCase):
+    def test_is_violated(self):
+        teams = [
+            Team(1, 'xyz', 'A'),
+            Team(2, 'xyz', 'A'),
+            Team(3, 'xyz', 'A'),
+            Team(4, 'xyz', 'A'),
+            Team(5, 'xyz', 'A'),
+            Team(6, 'xyz', 'A'),
+        ]
+
+        for index in range(len(teams)):
+            teams[index].assigned_index = index
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 7, 5],
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        # then
+        shared_venue_constraint = SharedVenueConstraint(shared_venue_team_pairs=[(1, 3), (2, 4)])
+        self.assertFalse(shared_venue_constraint.is_violated(teams, fixture_table))
+
+        shared_venue_constraint = SharedVenueConstraint(shared_venue_team_pairs=[(3, 1), (4, 2)])
+        self.assertFalse(shared_venue_constraint.is_violated(teams, fixture_table))
+
+        shared_venue_constraint = SharedVenueConstraint(shared_venue_team_pairs=[(1, 4)])
+        self.assertTrue(shared_venue_constraint.is_violated(teams, fixture_table))
+
+        shared_venue_constraint = SharedVenueConstraint(shared_venue_team_pairs=[(2, 3)])
+        self.assertTrue(shared_venue_constraint.is_violated(teams, fixture_table))
+
+        shared_venue_constraint = SharedVenueConstraint(shared_venue_team_pairs=[(1, 5)])
+        self.assertTrue(shared_venue_constraint.is_violated(teams, fixture_table))
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 6, 5],
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        shared_venue_constraint = SharedVenueConstraint(shared_venue_team_pairs=[(1, 3), (2, 4)])
+        self.assertTrue(shared_venue_constraint.is_violated(teams, fixture_table))
