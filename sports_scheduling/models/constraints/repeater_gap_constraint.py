76e1a30c7c097e5783568488cad2d16c3694b06a
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-11-28 23:33:28 +0100

    Implement and test repeater gap constraint

--- sports_scheduling/models/constraints/repeater_gap_constraint.py
+++ sports_scheduling/models/constraints/repeater_gap_constraint.py
@@ -1,4 +1,10 @@
+from typing import List
+
+from numpy import ndarray
+
 from sports_scheduling.models.constraints.base_constraint import BaseConstraint
+from sports_scheduling.models.teams.teams import Team
+from sports_scheduling.util import get_team_by_id
 
 
 class RepeaterGapConstraint(BaseConstraint):
@@ -11,5 +17,17 @@ class RepeaterGapConstraint(BaseConstraint):
         self.minimum_gap = minimum_gap
         self.penalty = penalty
 
-    def is_violated(self):
-        pass
+    def is_violated(self, teams: List[Team], fixture_table: ndarray) -> bool:
+        team_1 = get_team_by_id(teams, self.team1_id)
+        team_2 = get_team_by_id(teams, self.team2_id)
+        assert getattr(team_1, 'assigned_index') is not None and getattr(team_2, 'assigned_index') is not None, \
+            f"either team with id {team_1.id} or team with id {team_2.id} have no assigned_index property"
+
+        first_game = fixture_table[team_1.assigned_index, team_2.assigned_index]
+        second_game = fixture_table[team_2.assigned_index, team_1.assigned_index]
+
+        if abs(first_game - second_game) < self.minimum_gap:
+            # If there are less than `self.minimum_gap` matchweeks between the two matches of the two teams, the constraint is violated
+            return True
+        else:
+            return False
---
+++ tests/unit/repeater_gap_constraint_tests.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+
+import numpy as np
+
+from sports_scheduling.models.constraints.repeater_gap_constraint import RepeaterGapConstraint
+from sports_scheduling.models.teams.teams import Team
+
+
+class RepeaterGapConstraintTests(TestCase):
+    def test_is_violated(self):
+        teams = [
+            Team(1, 'xyz', 'A'),
+            Team(2, 'xyz', 'A'),
+            Team(3, 'xyz', 'A'),
+            Team(4, 'xyz', 'A'),
+            Team(5, 'xyz', 'A'),
+            Team(6, 'xyz', 'A'),
+        ]
+
+        for index in range(len(teams)):
+            teams[index].assigned_index = index
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 7, 5],
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        # then
+        repeater_gap_constraint = RepeaterGapConstraint(team1_id=1, team2_id=4, minimum_gap=5, penalty=1)
+        self.assertFalse(repeater_gap_constraint.is_violated(teams, fixture_table))
+
+        repeater_gap_constraint = RepeaterGapConstraint(team1_id=1, team2_id=4, minimum_gap=1, penalty=1)
+        self.assertFalse(repeater_gap_constraint.is_violated(teams, fixture_table))
+
+        repeater_gap_constraint = RepeaterGapConstraint(team1_id=1, team2_id=4, minimum_gap=10, penalty=1)
+        self.assertTrue(repeater_gap_constraint.is_violated(teams, fixture_table))
+
+        repeater_gap_constraint = RepeaterGapConstraint(team1_id=1, team2_id=4, minimum_gap=6, penalty=1)
+        self.assertTrue(repeater_gap_constraint.is_violated(teams, fixture_table))
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 7, 5],
+            [8, 5, 6, 0, 3, 2],
+            # ^ 9 turned to 8
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        # then
+        repeater_gap_constraint = RepeaterGapConstraint(team1_id=1, team2_id=4, minimum_gap=5, penalty=1)
+        self.assertTrue(repeater_gap_constraint.is_violated(teams, fixture_table))
+
+        repeater_gap_constraint = RepeaterGapConstraint(team1_id=1, team2_id=4, minimum_gap=4, penalty=1)
+        self.assertFalse(repeater_gap_constraint.is_violated(teams, fixture_table))
+
+        repeater_gap_constraint = RepeaterGapConstraint(team1_id=1, team2_id=4, minimum_gap=6, penalty=1)
+        self.assertTrue(repeater_gap_constraint.is_violated(teams, fixture_table))
