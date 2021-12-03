d709eab15f86efc9f1ad468cfdd3e9baf35017b0
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-12-03 01:27:55 +0100

    Improve README.md

--- tests/integration/sport_scheduling_integration_tests.py
+++ tests/integration/sport_scheduling_integration_tests.py
@@ -9,19 +9,19 @@ from sports_scheduling.util import show_fixture_list
 class SportSchedulingTests(TestCase):
 
     def test_sports_scheduling_6(self, _):
-        scheduler = Scheduler(6, 2)
+        scheduler = Scheduler(number_of_teams=6, number_of_shared_venue_pairs=2)
         scheduler.generate()
-        print(scheduler.fixture_table)
-        # show_fixture_list(scheduler.fixture_table)
+        # print(scheduler.fixture_table)
+        show_fixture_list(scheduler.fixture_table)
 
     def test_sports_scheduling_10(self, _):
         scheduler = Scheduler(10, 2)
         scheduler.generate()
-        print(scheduler.fixture_table)
+        # print(scheduler.fixture_table)
         show_fixture_list(scheduler.fixture_table)
 
     def test_sports_scheduling_20(self, _):
         scheduler = Scheduler(20, 3)
         scheduler.generate()
-        print(scheduler.fixture_table)
+        # print(scheduler.fixture_table)
         show_fixture_list(scheduler.fixture_table)
