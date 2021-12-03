5893981aed0b76ea0b693f20f5618197101d48fd
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-12-03 11:10:21 +0100

    Implement CompleteCycle constraints

--- sports_scheduling/models/constraints/complete_cycle_constraint.py
+++ sports_scheduling/models/constraints/complete_cycle_constraint.py
@@ -1,4 +1,9 @@
+from typing import List
+
+from numpy.core.records import ndarray
+
 from sports_scheduling.models.constraints.base_constraint import BaseConstraint
+from sports_scheduling.models.teams.teams import Team
 
 
 class CompleteCycleConstraint(BaseConstraint):
@@ -7,5 +12,17 @@ class CompleteCycleConstraint(BaseConstraint):
         """Each team should play once against each other team before playing a team for the second time"""
         super().__init__(bracket='completeCycleConstraint', level='HARD')
 
-    def is_violated(self):
-        pass
+    def is_violated(self, teams: List[Team], fixture_table: ndarray) -> bool:
+        first_half_season_matchweeks = [i for i in range(len(teams))]
+        for team in teams:
+            for i in range(len(teams)):
+                if fixture_table[team.assigned_index, i] == 0:
+                    continue
+
+                if fixture_table[team.assigned_index, i] in first_half_season_matchweeks and \
+                        fixture_table[i, team.assigned_index] in first_half_season_matchweeks:
+                    # If a team plays twice against another team in the matchweeks of the first half of the season, it means a team has
+                    # failed to meet every team in the first half of the season and has instead played twice against the same team.
+                    return True
+
+        return False
---
+++ tests/unit/complete_cycle_constraints.py
@@ -0,0 +1,80 @@
+from unittest import TestCase
+
+import numpy as np
+
+from sports_scheduling.models.constraints.complete_cycle_constraint import CompleteCycleConstraint
+from sports_scheduling.models.teams.teams import Team
+
+
+class CompleteCycleConstraintTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.complete_cycle_constraint = CompleteCycleConstraint()
+
+    def test_is_violated(self):
+        teams = [
+            Team(1, 'xyz', 'A'),
+            Team(2, 'xyz', 'A'),
+            Team(3, 'xyz', 'A'),
+            Team(4, 'xyz', 'A'),
+            Team(5, 'xyz', 'A'),
+            Team(6, 'xyz', 'A'),
+        ]
+
+        for index in range(len(teams)):
+            teams[index].assigned_index = index
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 7, 5],
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        self.assertFalse(self.complete_cycle_constraint.is_violated(teams, fixture_table))
+
+        # Team 0 plays against team 2 in matchweek 3 & 4. Consequently, it plays against team 3 in matchweek 8 and 9
+        fixture_table = np.array([
+            [0, 2, 4, 8, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 7, 5],
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        self.assertTrue(self.complete_cycle_constraint.is_violated(teams, fixture_table))
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 3, 6],
+            #            ^ Changed to 3
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 7, 5],
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        self.assertTrue(self.complete_cycle_constraint.is_violated(teams, fixture_table))
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 1, 7, 5],
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 10, 3, 4, 7, 0],
+            # Team 5 plays Team 2 in matchweek 3 & 5. Also T5 plays T4 in matchweeks 7 & 9.
+        ])
+        self.assertTrue(self.complete_cycle_constraint.is_violated(teams, fixture_table))
+
+        fixture_table = np.array([
+            [0, 2, 8, 4, 10, 6],
+            [7, 0, 4, 10, 1, 8],
+            [3, 9, 0, 5, 7, 1],
+            # Team 2 plays against T3 in matchweeks 5 & 6.
+            [9, 5, 6, 0, 3, 2],
+            [5, 6, 2, 8, 0, 9],
+            [1, 3, 10, 7, 4, 0],
+        ])
+        self.assertFalse(self.complete_cycle_constraint.is_violated(teams, fixture_table))
