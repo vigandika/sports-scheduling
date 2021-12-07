ee79a50ccc515e3bcf4130f0f71247a83e56bcc7
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-12-07 21:00:50 +0100

    Improve util module * Layout main application flow

--- sports_scheduling/main.py
+++ sports_scheduling/main.py
@@ -14,12 +14,12 @@ simulated_annealing(initial_solution, teams, soft_constraints, hard_constraints)
 
 """
 
-from parse_data import teams, soft_constraints, hard_constraints  # These will be taken from the request
+from parse_data import teams, hard_constraints  # These will be taken from the request
 from sports_scheduling.models.constraints.shared_venue_constraint import SharedVenueConstraint
 from sports_scheduling.scheduler import Scheduler
-from sports_scheduling.simulated_annealing import SimulatedAnnealing
+from sports_scheduling.util import assign_teams, show_fixture_list
 
-shared_venue_constraints = [soft_constraint for soft_constraint in soft_constraints if isinstance(soft_constraint, SharedVenueConstraint)]
+shared_venue_constraints = [hard_constraint for hard_constraint in hard_constraints if isinstance(hard_constraint, SharedVenueConstraint)]
 
 # Extract the number of shared venue team pairs
 if len(shared_venue_constraints) > 1:
@@ -27,10 +27,15 @@ if len(shared_venue_constraints) > 1:
 elif len(shared_venue_constraints) == 1:
     shared_venue_constraint = shared_venue_constraints[0]
 else:
-    shared_venue_constraint = 0
+    shared_venue_constraint = []
 
-initial_solution = Scheduler(number_of_teams=len(teams), shared_venue_team_pairs=shared_venue_constraint.shared_venue_team_pairs)
-
+# Create initial solution
+scheduler = Scheduler(number_of_teams=len(teams), shared_venue_team_pairs=shared_venue_constraint.shared_venue_team_pairs)
+scheduler.generate()
+initial_solution = scheduler.fixture_table
 # assign indexes
+assign_teams(teams, shared_venue_constraint.shared_venue_team_pairs)
+# Show fixture list
+show_fixture_list(initial_solution)
 
-SimulatedAnnealing.solve(initial_solution.fixture_table, teams, hard_constraints, soft_constraints)
+# SimulatedAnnealing.solve(initial_solution.fixture_table, teams, hard_constraints, soft_constraints)
--- sports_scheduling/util.py
+++ sports_scheduling/util.py
@@ -1,7 +1,7 @@
 import logging
 import math
 import random
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Dict
 
 import numpy as np
 from numpy import ndarray
@@ -9,20 +9,6 @@ from numpy import ndarray
 from sports_scheduling.models.teams.teams import Team
 
 logger = logging.getLogger(__name__)
-
-
-def show_fixture_list(fixture_table: ndarray):
-    no_of_games_per_round = len(fixture_table) // 2
-    for match_week in range((len(fixture_table) - 1) * 2):
-        item_index = np.where(fixture_table == match_week + 1)
-        match_week_fixtures = f"""
-            MATCHWEEK {match_week + 1}:
-        """
-
-        for game in range(no_of_games_per_round):
-            match_week_fixtures = f"{match_week_fixtures}\n\t\t\t\t{item_index[0][game] + 1} - {item_index[1][game] + 1}"
-
-        print(match_week_fixtures)
 
 
 def get_team_by_id(teams: List[Team], id: int) -> Team:
@@ -59,33 +45,54 @@ def get_indexes_of_shared_venue_teams(number_of_teams: int, number_of_shared_ven
     return indexes_of_shared_venue_teams
 
 
+team_index_mapping: Dict[int, Team] = {}
+
+
 def assign_teams(teams: List[Team], shared_venue_team_pairs: List[Tuple[int, int]]):
     available_indexes = [i for i in range(len(teams))]
-    global indexes_of_shared_venue_teams
     number_of_shared_venue_team_pairs = len(shared_venue_team_pairs)
-    # assign shared venue teams first
+    # assign shared venue teams first if there are any
     if number_of_shared_venue_team_pairs > 0:
-        # If there are shared venue teams in the problem
-        assert len(indexes_of_shared_venue_teams) > 0, 'shared venue teams applicable but indexes are not specified'
+        assert indexes_of_shared_venue_teams, 'shared venue teams applicable but indexes are not specified'
         for index, pair in enumerate(shared_venue_team_pairs):
             logger.info(f'setting shared venue teams with IDs: {shared_venue_team_pairs} indexes {indexes_of_shared_venue_teams}')
 
+            # First element of the pair
             team = get_team_by_id(teams, pair[0])
-            logger.debug(f'setting index {indexes_of_shared_venue_teams[index][0]} to team with ID {team.id}')
-            team.assigned_index = indexes_of_shared_venue_teams[index][0]
-            available_indexes.remove(indexes_of_shared_venue_teams[index][0])
+            index_to_assign = indexes_of_shared_venue_teams[index][0]
+            __assign_team_to_index(team, index_to_assign)
+            available_indexes.remove(index_to_assign)
 
+            # Second element of the pair
             team = get_team_by_id(teams, pair[1])
-            logger.debug(f'setting index {indexes_of_shared_venue_teams[index][1]} to team with ID {team.id}')
-            team.assigned_index = indexes_of_shared_venue_teams[index][1]
-            available_indexes.remove(indexes_of_shared_venue_teams[index][1])
+            index_to_assign = indexes_of_shared_venue_teams[index][1]
+            __assign_team_to_index(team, index_to_assign)
+            available_indexes.remove(index_to_assign)
 
     for team in teams:
         # Randomly set other indexes
         if team.assigned_index is None:
             random_index = random.choice(available_indexes)
-            logger.debug(f'setting index {random_index} to team with ID {team.id}')
-            team.assigned_index = random_index
+            __assign_team_to_index(team, random_index)
             available_indexes.remove(random_index)
 
-    print([vars(team) for team in teams])
+
+def __assign_team_to_index(team: Team, index_to_assign: int):
+    logger.debug(f'setting index {index_to_assign} to team with ID {team.id}')
+    team.assigned_index = index_to_assign
+    team_index_mapping[index_to_assign] = team
+
+
+def show_fixture_list(fixture_table: ndarray):
+    assert team_index_mapping, '`team_index_mapping` is empty. Method `assign_teams` should be executed first to assign teams to an index'
+    no_of_games_per_round = len(fixture_table) // 2
+    for matchweek in range((len(fixture_table) - 1) * 2):
+        # get coordinates ((x1,x2,xn...), (y1,y2,yn...)) where condition
+        matchweek_coordinates = np.where(fixture_table == matchweek + 1)
+        matchweek_fixtures = f"""
+            MATCHWEEK {matchweek + 1}:
+        """
+        for game in range(no_of_games_per_round):
+            matchweek_fixtures = f"{matchweek_fixtures}\n\t\t{team_index_mapping[matchweek_coordinates[0][game]].name} - {team_index_mapping[matchweek_coordinates[1][game]].name}"
+
+        print(matchweek_fixtures)
